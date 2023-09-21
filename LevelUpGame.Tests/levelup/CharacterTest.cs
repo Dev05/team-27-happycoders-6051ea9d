ba33bbde675bdd781051970818a2b4f6e1fb222e
Dev <devjyoti.datta@hotmail.com>
2023-09-21 20:36:26 +0000

    character update

--- LevelUpGame.Tests/levelup/CharacterTest.cs
+++ LevelUpGame.Tests/levelup/CharacterTest.cs
@@ -9,11 +9,56 @@ namespace levelup
 
          [Test]
          public void SetCharacterName(){
-
             var testObj= new Character("Terminator");
             Assert.IsNotNull(testObj.Name);
             Assert.AreEqual(testObj.Name,"Terminator");
          }
 
+          [Test]
+            public void SetCharacterNameNull(){
+            var testObj= new Character(null);
+            Assert.IsNotNull(testObj.Name);
+            Assert.AreEqual(testObj.Name,"Character");
+         }
+
+          [Test]
+            public void SetCharacterNameNotDefault(){
+            var testObj= new Character("abc");
+            Assert.IsNotNull(testObj.Name);
+            Assert.AreNotEqual(testObj.Name,"Character");
+         }
+
+
+         [Test]
+         public void MoveUp(){
+            var testObj = new Character("Terminator");
+            //Default position is 5, 5
+            bool validMove = testObj.Move("UP");
+            Assert.IsTrue(validMove);
+         }
+
+         [Test]
+         public void MoveDown(){
+            var testObj = new Character("Terminator");
+            //Default position is 5, 5
+            bool validMove = testObj.Move("DOWN");
+            Assert.IsTrue(validMove);
+         }
+
+        [Test]
+         public void MoveRight(){
+           var testObj = new Character("Terminator");
+            //Default position is 5, 5
+            bool validMove = testObj.Move("RIGHT");
+            Assert.IsTrue(validMove);
+         }
+
+         [Test]
+         public void MoveLeft(){
+           var testObj = new Character("Terminator");
+            //Default position is 5, 5
+            bool validMove = testObj.Move("LEFT");
+            Assert.IsTrue(validMove);
+         }
     }
 }
--- LevelUpGame/levelup/Character.cs
+++ LevelUpGame/levelup/Character.cs
@@ -5,9 +5,29 @@ namespace levelup
     public class Character{
 
     public String Name{get;set;}
+    public Position currentPosition {get;set;}
+
+    private Position GetPosition(){
+        return currentPosition;
+    }
+
+    private void SetPosition(Position position){
+        currentPosition = position;
+    }
 
     public Character(String name){
-        Name=name;
+        Name=name??"Character";
+        currentPosition.x = 5;
+        currentPosition.y = 5;
+    }
+
+    public bool Move(String direction){
+        
+        CASE direction:
+            "UP": currentPosition.y++; break;
+            "DOWN": currentPosition.y--; break;
+            "LEFT": currentPosition.x--; break;
+            "RIGHT": currentPosition.x++; break;
     }
 
     }
