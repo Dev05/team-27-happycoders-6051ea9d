38d5bfe9a3ee37d101160bb4ae5a37cea31d7f13
Dev <devjyoti.datta@hotmail.com>
2023-09-22 14:26:22 +0000

    added character class method

--- LevelUpGame.Tests/levelup/CharacterTest.cs
+++ LevelUpGame.Tests/levelup/CharacterTest.cs
@@ -15,8 +15,8 @@ namespace levelup
          }
 
           [Test]
-            public void SetCharacterNameNullTest(){
-            var testObj= new Character(null);
+            public void SetCharacterNameEmptyTest(){
+            var testObj= new Character("");
             Assert.IsNotNull(testObj.Name);
             Assert.AreEqual(testObj.Name,"Character");
          }
@@ -49,6 +49,50 @@ namespace levelup
             testObj.EnterMap(map);        
             Assert.IsNotNull(testObj.currentMap);
          }
+
+          [Test]
+            public void GetPositionTest(){
+            var testObj= new Character();
+            var pos=testObj.GetPosition();        
+            Assert.AreEqual(pos.X,5);
+            Assert.AreEqual(pos.Y,5);
+         }
+
+          [Test]
+            public void MoveNorthTest(){
+            var testObj= new Character();        
+            testObj.Move(DIRECTION.NORTH);   
+            Position currPos= testObj.GetPosition();
+            Assert.AreEqual(currPos.X,5);
+             Assert.AreEqual(currPos.Y,6);
+         }     
+
+          [Test]
+            public void MoveSouthhTest(){
+            var testObj= new Character();        
+            testObj.Move(DIRECTION.SOUTH);   
+            Position currPos= testObj.GetPosition();
+            Assert.AreEqual(currPos.X,5);
+             Assert.AreEqual(currPos.Y,4);
+         }     
+
+          [Test]
+            public void MoveEastTest(){
+            var testObj= new Character();        
+            testObj.Move(DIRECTION.EAST);   
+            Position currPos= testObj.GetPosition();
+            Assert.AreEqual(currPos.X,6);
+             Assert.AreEqual(currPos.Y,5);
+         }     
+
+          [Test]
+            public void MoveWestTest(){
+            var testObj= new Character();        
+            testObj.Move(DIRECTION.WEST);   
+            Position currPos= testObj.GetPosition();
+            Assert.AreEqual(currPos.X,4);
+             Assert.AreEqual(currPos.Y,5);
+         }     
          
     }
 }
--- LevelUpGame/levelup/Character.cs
+++ LevelUpGame/levelup/Character.cs
@@ -7,15 +7,17 @@ namespace levelup
     public String Name{get;set;}
     private String DefaultName="Character";
     public Position currentPosition {get;set;}
-    public Map currentMap;
+    public Map? currentMap;
 
 
     public Character(String name){ 
-        Name=name??DefaultName; 
+        Name=(name==null || name.Length==0)?DefaultName:name; 
+        currentPosition= new Position(5,5);
     }
 
     public Character(){ 
         Name=DefaultName; 
+        currentPosition= new Position(5,5);
     }    
     
     public Position GetPosition(){
@@ -30,6 +32,9 @@ namespace levelup
         currentMap=map;
     }
 
+    public void Move(DIRECTION dir){
+        //currentPosition=currentMap.CalculatePosition(currentPosition,dir);
+    }
 
     }
 }
